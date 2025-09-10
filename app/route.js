61d9b3aac7ab3afb6337a6e4d27bf03d9e09563c
Shiyue00 <94933592+Shiyue00@users.noreply.github.com>
2025-09-10 11:29:34 +0800

    Update route.js

--- app/route.js
+++ app/route.js
@@ -1,10 +1,6 @@
-// Этот файл обрабатывает GET-запросы к корневому URL-адресу ("/") вашего сайта.
 export async function GET(request) {
-  // Возвращает ответ "Hello World!" с кодом состояния 200 (OK).
   return new Response('Hello World!', {
     status: 200,
-    headers: {
-      'Content-Type': 'text/plain',
-    },
+    headers: { 'Content-Type': 'text/plain' },
   });
 }
