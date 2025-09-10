a860c7e8172684350047f980a474ab241e8e4142
Shiyue00 <94933592+Shiyue00@users.noreply.github.com>
2025-09-10 13:55:29 +0800

    Delete instrumentation.js

--- instrumentation.js
+++
@@ -1,8 +0,0 @@
-import 'server-only';
-import { runAgent } from './lib/agent.js';  
-
-export async function register() {
-  // 这段代码只会在服务器启动时运行一次
-  console.log('[INSTRUMENTATION] Server starting up, registering agent...');
-  runAgent();
-}
