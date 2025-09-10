a1a5056f97733f318c024ccca9d5f4c7498da0d9
Shiyue00 <94933592+Shiyue00@users.noreply.github.com>
2025-09-10 13:38:05 +0800

    Create instrumentation.js

---
+++ instrumentation.js
@@ -0,0 +1,7 @@
+import { runAgent } from './lib/agent.js';
+
+export async function register() {
+  // 这段代码只会在服务器启动时运行一次
+  console.log('[INSTRUMENTATION] Server starting up, registering agent...');
+  runAgent();
+}
